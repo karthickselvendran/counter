e7f766155666edd8ffa29008d27f4ce5083fa900
karthickselvendran <selvendrankarthick@gmail.com>
2023-05-28 17:38:37 +0530

    code optimization

--- src/App.js
+++ src/App.js
@@ -6,20 +6,33 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  console.log("count :: ", count)
+  console.log("count :: ", count);
 
   return (
     <div>
       <Header title="Counter Application" />
       <div className="container">
         <p>
-          Welcome to our platform.  This website has a feature for incrementing, decrementing and resetting numbers.
+          Welcome to our platform. This website has a feature for incrementing,
+          decrementing and resetting numbers.
         </p>
         <span className="count">{count}</span>
         <div className="buttonGroup">
-          <Button className="bgColorPositive" name="Increment" onClick={() => setCount(count => count + 1)} />
-          <Button className="bgColorNeutral" name="Reset" onClick={() => setCount(0)} />
-          <Button className="bgColorNegative" name="Decrement" onClick={() => setCount(count => count - 1)} />
+          <Button
+            className="bgColorPositive"
+            name="Increment"
+            onClick={() => setCount((prevCount) => prevCount + 1)}
+          />
+          <Button
+            className="bgColorNeutral"
+            name="Reset"
+            onClick={() => setCount(0)}
+          />
+          <Button
+            className="bgColorNegative"
+            name="Decrement"
+            onClick={() => setCount((prevCount) => prevCount - 1)}
+          />
         </div>
       </div>
     </div>
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./button.css";
 
 const Button = (props) => {
-  const { name = "", onClick = {} } = props;
+  const { name = "Submit", onClick = () => {} } = props;
   return (
     <div>
       <button onClick={(e) => onClick(e)} {...props}>
-        {name || "Submit"}
+        {name}
       </button>
     </div>
   );
